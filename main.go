bb0d77b3ddc4806650f48f2ece449c7cdeae68cf
tidao <tidao2049@gmail.com>
2020-11-24 15:35:47 +0800

    add main.go file and add traverse all blocks to find the transactions

--- main.go
+++ main.go
@@ -1,52 +1,81 @@
 /***********************************************************************
 # File Name: main.go
 # Author: TiDao
-# mail: songzhaofeng411@gmail.com
-# Created Time: 2020-09-09 15:09:28
+# mail: tidao2049@gmail.com
+# Created Time: 2020-11-24 12:28:58
 *********************************************************************/
 package main
+import "log"
+import "./cita"
+import "strconv"
+import "sync"
 
-import(
-    "encoding/json"
-    "./cita"
-    "fmt"
-//    "os"
-    "bytes"
-    "log"
-)
+func TestGetBlock(){
+    var url = "http://192.168.1.65:1337"
+//    req := &cita.Request{
+//        Jsonrpc: "2.0",
+//        Method: "getBlockByNumber",
+//        Params: []interface{}{
+//            "",
+//            true,
+//        },
+//        Id: 1,
+//    }
+//    result := new(cita.ResultBlock)
+
+    var wg sync.WaitGroup
+    ch := make(chan string,20)
+//    for i := 0x0;i<100000000000;i++{
+//        req.Params[0] = "0x"+strconv.FormatInt(<-ch,16)
+//        log.Printf("the block height is %s",req.Params[0])
+//        err := cita.GetBlock(req,result,url)
+//        if err != nil{
+//            log.Println(err)
+//        }
+//        if len(result.Body.Transactions) != 0 {
+//            log.Println(*result)
+//        }
+//        time.Sleep(time.Second)
+//    }
+    go func(){
+        for i := 0x0;i<1000000000;i++{
+            ch <- "0x"+strconv.FormatInt(int64(i),16)
+        }
+    }()
+    for i:=0;i<20;i++{
+        wg.Add(1)
+        req := &cita.Request{
+            Jsonrpc: "2.0",
+            Method: "getBlockByNumber",
+            Params: []interface{}{
+                "",
+                true,
+            },
+            Id: 1,
+        }
+        result := new(cita.ResultBlock)
+        go func(){
+            for {
+                req.Params[0] = <-ch 
+                //log.Printf("the block height is %s",req.Params[0])
+                err := cita.GetBlock(req,result,url)
+                if err != nil{
+                    log.Panic(err)
+                }
+                if len(result.Body.Transactions) != 0 {
+                    for _,Transaction := range result.Body.Transactions{
+                        log.Println(Transaction)
+                        log.Printf("\n\n\n")
+                    }
+                }
+            }
+        }()
+    }
+
+    wg.Wait()
+
+}
 
 func main(){
-    url := "http://192.168.1.65:1337"
-    request := &cita.Request{
-        Jsonrpc: "2.0",
-        Method: "getMetaData",
-        Params: []interface{}{"latest"},
-        Id: 1,
-    }
-
-    requestJson,err := json.Marshal(request)
-    if err != nil{
-        fmt.Println(err)
-    }
-    responseJson := &[]byte{}
-
-    *responseJson,err = cita.Post(url,requestJson)
-    if err != nil{
-        log.Fatal(err)
-    }
-
-    var response = &cita.Response{}
-
-    err = json.Unmarshal(*responseJson,response)
-    if err != nil{
-        fmt.Println(err)
-    }
-
-    fmt.Println(string(*responseJson))
-
-    var out bytes.Buffer
-
-    err = json.Indent(&out,*responseJson,"","\t")
-    fmt.Println(out.String())
-    //out.WriteTo(os.Stdout)
+    TestGetBlock()
 }
