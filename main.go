df2739a8184581a21bf81e6d340bc58380b59e93
tidao <tidao2049@gmail.com>
2020-11-24 17:39:15 +0800

    nothing

--- main.go
+++ main.go
@@ -12,31 +12,10 @@ import "sync"
 
 func TestGetBlock(){
     var url = "http://192.168.1.65:1337"
-//    req := &cita.Request{
-//        Jsonrpc: "2.0",
-//        Method: "getBlockByNumber",
-//        Params: []interface{}{
-//            "",
-//            true,
-//        },
-//        Id: 1,
-//    }
-//    result := new(cita.ResultBlock)
 
     var wg sync.WaitGroup
     ch := make(chan string,20)
-//    for i := 0x0;i<100000000000;i++{
-//        req.Params[0] = "0x"+strconv.FormatInt(<-ch,16)
-//        log.Printf("the block height is %s",req.Params[0])
-//        err := cita.GetBlock(req,result,url)
-//        if err != nil{
-//            log.Println(err)
-//        }
-//        if len(result.Body.Transactions) != 0 {
-//            log.Println(*result)
-//        }
-//        time.Sleep(time.Second)
-//    }
+    TransactionCh := make(chan string,20)
     go func(){
         for i := 0x0;i<1000000000;i++{
             ch <- "0x"+strconv.FormatInt(int64(i),16)
@@ -51,7 +30,7 @@ func TestGetBlock(){
                 "",
                 true,
             },
-            Id: 1,
+            Id: int32(i),
         }
         result := new(cita.ResultBlock)
         go func(){
@@ -64,13 +43,39 @@ func TestGetBlock(){
                 }
                 if len(result.Body.Transactions) != 0 {
                     for _,Transaction := range result.Body.Transactions{
-                        log.Println(Transaction)
-                        log.Printf("\n\n\n")
+                        TransactionCh <- Transaction.Hash
                     }
                 }
             }
         }()
     }
+
+    go func(){
+        req := &cita.Request{
+            Jsonrpc: "2.0",
+            Method: "getTransaction",
+            Params: []interface{}{
+                "",
+            },
+            Id: 1,
+        }
+
+        result := &cita.ResultTransaction{}
+        for {
+            req.Params[0] = <-TransactionCh
+            err := cita.GetTransaction(req,result,url)
+            if err != nil{
+                log.Panic(err)
+            }
+            log.Printf("hash: %s",result.Hash)
+            log.Printf("content: %s",result.Content)
+            log.Printf("from: %s\n",result.From)
+            log.Printf("blockNumber: %s\n",result.BlockNumber)
+            log.Printf("blockHash: %s\n",result.BlockHash)
+            log.Printf("index: %s\n\n\n\n",result.Index)
+        }
+    }()
+
 
     wg.Wait()
 
