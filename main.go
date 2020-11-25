acee89e64f3920e693a8e665ceb93222521a18a2
tidao <tidao2049@gmail.com>
2020-11-25 19:29:54 +0800

    have unsafe pointer problem

--- main.go
+++ main.go
@@ -7,8 +7,11 @@
 package main
 import "log"
 import "./cita"
-import "strconv"
+//import "strconv"
+import "io"
 import "sync"
+import "time"
+import "strings"
 
 func TestGetBlock(){
     var url = "http://192.168.1.65:1337"
@@ -16,9 +19,38 @@ func TestGetBlock(){
     var wg sync.WaitGroup
     ch := make(chan string,20)
     TransactionCh := make(chan string,20)
+
     go func(){
-        for i := 0x0;i<1000000000;i++{
-            ch <- "0x"+strconv.FormatInt(int64(i),16)
+        //for i := 0x1000000;i<1000000000;i++{
+        //    ch <- "0x"+strconv.FormatInt(int64(i),16)
+        //    log.Println("starting block number grow")
+        //}
+        req := &cita.Request{
+            Jsonrpc: "2.0",
+            Method: "blockNumber",
+            Params: []interface{}{},
+            Id: 1,
+        }
+        var Error cita.Error
+        var err   error
+        //var lock sync.Mutex
+        var blockNumber string
+        var temp string
+        for {
+            Error,err = cita.BlockNumber(req,&blockNumber,url)
+
+
+            if err != nil || Error.Code != 0 {
+                log.Println(err)
+                log.Println(Error)
+            }
+
+            if blockNumber != temp{
+                temp = blockNumber
+                ch <- temp
+                log.Println(blockNumber)
+            }
+            time.Sleep(time.Second*1)
         }
     }()
     for i:=0;i<30;i++{
@@ -37,9 +69,15 @@ func TestGetBlock(){
             for {
                 req.Params[0] = <-ch
                 log.Printf("the block height is %s",req.Params[0])
+                log.Println(req)
                 Error,err := cita.GetBlock(req,result,url)
+                log.Println(result)
                 if err != nil{
-                    log.Panic(err)
+                    if err == io.EOF{
+                        log.Printf("the err is EOF")
+                    }else{
+                        log.Panic(err)
+                    }
                 }
                 if Error.Code == 0 {
                     if len(result.Body.Transactions) != 0 {
@@ -47,6 +85,8 @@ func TestGetBlock(){
                             TransactionCh <- Transaction.Hash
                         }
                     }
+                }else{
+                    log.Println("get Block error:",Error)
                 }
                 //if len(result.Body.Transactions) != 0 {
                 //    for _,Transaction := range result.Body.Transactions{
@@ -83,9 +123,7 @@ func TestGetBlock(){
         }
     }()
 
-
     wg.Wait()
-
 }
 
 func main(){
